84a5c2738ced301ff9e869db6c04de9ad7ec6c70
warnakulasuriya-fds-e23 <fdswarnakulasuriya@gmail.com>
2025-08-11 10:04:01 +0530

    rename authenticateToken to verifyToken

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const authenticateToken = (req, res, next) => {
+const verifyToken = (req, res, next) => {
     const token = req.headers["authorization"]?.split(" ")[1]; 
     if (!token) {
         return res.status(401).json({ error: "Access token is missing" });
@@ -15,4 +15,4 @@ const authenticateToken = (req, res, next) => {
     });
 }
 
-module.exports = { authenticateToken };
+module.exports = { verifyToken };
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -3,11 +3,11 @@ const {
   login,
   removeAccount,
 } = require("../controllers/UserController");
-const {authenticateToken} = require("../middleware/authenticateToken");
+const {verifyToken} = require("../middleware/verifyToken");
 const express = require("express");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.delete("/remove-account", authenticateToken, removeAccount);
+router.delete("/remove-account", verifyToken, removeAccount);
 module.exports = router;
