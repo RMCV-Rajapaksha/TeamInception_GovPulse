9a1601a3641662e44db81b418319399c64330778
Isuru Sanjeewa Madusanka <143842041+ISMadusanka@users.noreply.github.com>
2025-08-13 20:42:33 +0530

    upvote aded

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,14 @@ const userRouter = require("./routes/UserRouter");
 const officialRouter = require("./routes/OfficialRouter");
 const timeSlotRouter = require("./routes/TimeSlotRouter");
 const generateImageSignatureRouter = require("./routes/GenerateImageSignatureRouter");
+const upvoteRouter = require("./routes/UpvoteRoute");
 app.use(cors());
 app.use(express.json());
 app.use("/api/issues", issueRouter);
 app.use("/api/users", userRouter);
 app.use("/api/officials", officialRouter);
 app.use("/api/time-slots", timeSlotRouter);
+app.use("/api/upvotes", upvoteRouter);
 
 app.use("/api/generate-image-signature", generateImageSignatureRouter);
 app.listen(port, () => {
