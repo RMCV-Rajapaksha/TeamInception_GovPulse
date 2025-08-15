5ebf7febf92aacebbd7a5b3b1f42d4073025e20c
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-08-15 17:15:13 +0530

    feat: add endpoint to retrieve available issue statuses for authority

--- backend/routes/v1/AuthorityRouter.js
+++ backend/routes/v1/AuthorityRouter.js
@@ -1,10 +1,20 @@
-const {getAuthorities} = require("../../controllers/v1/AuthorityController");
-const {verifyToken, verifyOfficialToken} = require("../../middleware/verifyToken");
+const {
+  getAuthorities,
+  getAvailableIssueStatusOfAuthority,
+} = require("../../controllers/v1/AuthorityController");
+const {
+  verifyToken,
+  verifyOfficialToken,
+} = require("../../middleware/verifyToken");
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/get-authorities-by-user", verifyToken, getAuthorities);
 router.get("/get-authorities-by-official", verifyOfficialToken, getAuthorities);
+router.get(
+  "/get-available-issue-status-of-authority",
+  verifyOfficialToken,
+  getAvailableIssueStatusOfAuthority
+);
 
 module.exports = router;
